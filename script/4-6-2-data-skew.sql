320cd45f4ffae872bc5276aefb4a59ec39a8591c
Constantinos (Costas) Antzakas <cantzakas@pivotal.io>
2019-07-29 17:02:02 +0100

    Update 4-6-2-data-skew.sql

--- script/4-6-2-data-skew.sql
+++ script/4-6-2-data-skew.sql
@@ -3,6 +3,6 @@ SELECT
 	skcc.skcrelname as tablename, 
 	skcc.skccoeff as coefficient 
 FROM
-	gp_toolkit.gp_skew_coefficients skcc
+	gp_toolkit.gp_skew_coefficients skcc 
 WHERE
 	skcc.skcnamespace || '.' || skcc.skcrelname = 'demo.amzn_reviews';
