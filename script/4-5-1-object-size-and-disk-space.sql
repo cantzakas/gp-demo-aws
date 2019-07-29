c5a4611bc0c65c1849079cb2d45dd42e8baf6fb2
Constantinos (Costas) Antzakas <cantzakas@pivotal.io>
2019-07-29 16:10:46 +0100

    Update 4-5-1-object-size-and-disk-space.sql

--- script/4-5-1-object-size-and-disk-space.sql
+++ script/4-5-1-object-size-and-disk-space.sql
@@ -1,3 +1,11 @@
-SELECT 'demo' AS schemaname, 'amzn_reviews' AS tablename, pg_size_pretty(pg_relation_size('demo.amzn_reviews')) AS size, 'Disk space used by the table or index.' AS level 
+SELECT
+	'demo' AS schemaname, 
+	'amzn_reviews' AS tablename, 
+	pg_size_pretty(pg_relation_size('demo.amzn_reviews')) AS size, 
+	'Disk space used by the table or index.' AS level 
 UNION ALL 
-SELECT 'demo' AS schemaname, 'amzn_reviews' AS tablename, pg_size_pretty(pg_total_relation_size('demo.amzn_reviews')) AS size, 'Total disk space used by the table, including indexes and toasted data.' AS level;
+SELECT
+	'demo' AS schemaname,
+	'amzn_reviews' AS tablename, 
+	pg_size_pretty(pg_total_relation_size('demo.amzn_reviews')) AS size, 
+	'Total disk space used by the table, including indexes and toasted data.' AS level;
