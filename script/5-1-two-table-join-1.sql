93779d367dc018f3876f1ad3d139a80d324a5f01
Constantinos (Costas) Antzakas <cantzakas@pivotal.io>
2019-07-31 14:28:05 +0100

    Create 5-1-two-table-join-1.sql

---
+++ script/5-1-two-table-join-1.sql
@@ -0,0 +1,24 @@
+CREATE TABLE demo.calendar (cdate DATE) 
+DISTRIBUTED BY (cdate);
+
+INSERT INTO demo.calendar (cdate) 
+SELECT GENERATE_SERIES(0, 364) + '2019-01-01'::DATE;
+
+CREATE TABLE demo.sales (cdate DATE, qty INT, revenue NUMERIC) 
+DISTRIBUTED BY (cdate);
+
+CREATE TABLE demo.sales2 (cdate DATE, qty INT, revenue NUMERIC) 
+DISTRIBUTED RANDOMLY;
+
+INSERT INTO demo.sales (cdate, qty, revenue) 
+SELECT (generate_series % 365) + '2019-01-01'::DATE, 
+	qty, 
+	qty * 19.99 
+FROM ( 
+	SELECT *,
+		TRUNC(RANDOM() * 50 + 1)::INT AS qty 
+	FROM 
+		GENERATE_SERIES(1, 1000000)) A;
+
+INSERT INTO demo.sales2 
+SELECT * FROM demo.sales;
