0806b4b9b0ff30e6e165ff187ab266089fa92f5f
Constantinos (Costas) Antzakas <cantzakas@pivotal.io>
2019-08-01 08:40:02 +0100

    Update 7-4-create-compressed-table-2.sql

--- script/7-4-create-compressed-table-2.sql
+++ script/7-4-create-compressed-table-2.sql
@@ -1,3 +1,5 @@
-CREATE TABLE foo (a int ENCODING (compresstype=zlib), 
-                  b text ENCODING (compresstype=quicklz, blocksize=65536)) 
-WITH (appendonly=true);
+CREATE TABLE foo ( 
+	a int ENCODING (compresstype=zlib), 
+	b text ENCODING (compresstype=quicklz, blocksize=65536)) 
+WITH (appendonly=true) 
+DISTRIBUTED BY (a);
