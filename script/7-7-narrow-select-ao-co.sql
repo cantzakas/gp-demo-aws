a6b70d9beb1281e10f2f561652653211da703f6b
Constantinos (Costas) Antzakas <cantzakas@pivotal.io>
2019-08-01 13:28:39 +0100

    Update and rename 7-7-narrow-select-ao-col.sql to 7-7-narrow-select-ao-co.sql

--- script/7-7-narrow-select-ao-co.sql
+++ script/7-7-narrow-select-ao-co.sql
@@ -3,7 +3,7 @@ SELECT product_id,
 	marketplace, 
 	product_category, 
 	star_rating 
-FROM demo.amzn_reviews_ao_ro_zlib3 
+FROM demo.amzn_reviews_ao_co_zlib3 
 WHERE DATE_PART('year', review_date) BETWEEN 2000 AND 2005 
 ORDER BY RANDOM() 
 LIMIT 2000;
