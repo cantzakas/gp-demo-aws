7bac187258ae86fa1e0527d2d976da3ae1a0d9c3
Constantinos (Costas) Antzakas <cantzakas@pivotal.io>
2019-08-01 09:22:32 +0100

    Update 7-5-amzn-reviews-ao-row-zlib3.sql

--- script/7-5-amzn-reviews-ao-row-zlib3.sql
+++ script/7-5-amzn-reviews-ao-row-zlib3.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS demo.amzn_reviews_ao;
+DROP TABLE IF EXISTS demo.amzn_reviews_ao_ro_zlib3; 
 
-CREATE TABLE demo.amzn_reviews(
+CREATE TABLE demo.amzn_reviews_ao_ro_zlib3( 
   marketplace TEXT, 
   customer_id BIGINT, 
-  review_id TEXT,
+  review_id TEXT, 
   product_id TEXT, 
   product_parent BIGINT, 
   product_title TEXT, 
@@ -16,5 +16,5 @@ CREATE TABLE demo.amzn_reviews(
   review_headline TEXT, 
   review_body TEXT, 
   review_date DATE) 
-WITH (appendonly=true, orientation=row, compresstype=zlib, compresslevel=3)
+WITH (appendonly=true, orientation=row, compresstype=zlib, compresslevel=3) 
 DISTRIBUTED BY (review_id);
