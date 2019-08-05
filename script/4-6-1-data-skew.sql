4367becfab3c587be3e0c54fec3265d31678cce8
Constantinos (Costas) Antzakas <cantzakas@pivotal.io>
2019-08-05 13:15:18 +0100

    Update 4-6-1-data-skew.sql

--- script/4-6-1-data-skew.sql
+++ script/4-6-1-data-skew.sql
@@ -1,5 +1,5 @@
 SELECT
-	'Example Table' AS "Table Name", 
+	'demo.amzn_reviews' AS "Table Name", 
 	max(c) AS "Max Seg Rows", min(c) AS "Min Seg Rows", 
 	(max(c)-min(c))*100.0/max(c) AS "Percentage Difference Between Max & Min" 
 FROM (
