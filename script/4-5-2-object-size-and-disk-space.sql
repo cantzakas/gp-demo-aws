ddb1af97097d3da39da77ec7774b950855d8fae7
Constantinos (Costas) Antzakas <cantzakas@pivotal.io>
2019-08-05 16:29:45 +0100

    Update 4-5-2-object-size-and-disk-space.sql

--- script/4-5-2-object-size-and-disk-space.sql
+++ script/4-5-2-object-size-and-disk-space.sql
@@ -1,44 +1,26 @@
-SELECT gp_size_of_table_disk.schemaname,
-	gp_size_of_table_disk.tablename, 
-	tabledisksize, 
-	uncompressedsize, 
-	tablesize, 
-	indexsize, 
-	toastsize, 
-	othersize 
-FROM (
-		SELECT sotd.sotdoid as oid, 
-			sotd.sotdschemaname as schemaname, 
-			sotd.sotdtablename as tablename, 
-			pg_size_pretty(sotd.sotdsize::BIGINT) as tablesize, 
-			pg_size_pretty(sotd.sotdtoastsize::BIGINT) as toastsize, 
-			pg_size_pretty(sotd.sotdadditionalsize::BIGINT) as othersize
-		FROM
-			gp_toolkit.gp_size_of_table_disk as sotd
-		WHERE
-			sotd.sotdschemaname || '.' || sotd.sotdtablename = 'demo.amzn_reviews') 
-	gp_size_of_table_disk, 
-	(
-		SELECT sotaid.sotaidoid as oid, 
-			sotaid.sotaidschemaname as schemaname, 
-			sotaid.sotaidtablename as tablename, 
-			pg_size_pretty(sotaid.sotaidtablesize::BIGINT) as tabledisksize, 
-			pg_size_pretty(sotaid.sotaididxsize::BIGINT) as indexsize
-		FROM
-			gp_toolkit.gp_size_of_table_and_indexes_disk as sotaid 
-		WHERE
-			sotaid.sotaidschemaname || '.' || sotaid.sotaidtablename = 'demo.amzn_reviews') 
-	gp_size_of_table_and_indexes_disk, 
-	(
-		SELECT sotu.sotuoid as oid, 
-			sotu.sotuschemaname as schemaname, 
-			sotu.sotutablename as tablename, 
-			pg_size_pretty(sotu.sotusize::BIGINT) as uncompressedsize
-		FROM
-			gp_toolkit.gp_size_of_table_uncompressed as sotu
-		WHERE
-			sotu.sotuschemaname || '.' || sotu.sotutablename = 'demo.amzn_reviews') 
-	gp_size_of_table_uncompressed 
-WHERE
-	gp_size_of_table_disk.oid = gp_size_of_table_and_indexes_disk.oid
-	AND gp_size_of_table_and_indexes_disk.oid = gp_size_of_table_uncompressed.oid;
+SELECT pg_namespace.nspname AS schema,
+	pg_class.relname AS relation, 
+	pg_size_pretty(sotd.sotdsize::BIGINT) as tablesize, 
+	pg_size_pretty(sotd.sotdtoastsize::BIGINT) as toastsize, 
+	pg_size_pretty(sotd.sotdadditionalsize::BIGINT) as othersize, 
+	pg_size_pretty(sotaid.sotaidtablesize::BIGINT) as tabledisksize, 
+	pg_size_pretty(sotaid.sotaididxsize::BIGINT) as indexsize, 
+	pg_size_pretty(sotu.sotusize::BIGINT) as uncompressedsize, 
+	100.0 * (1-(sotaid.sotaidtablesize/sotu.sotusize)) AS compressionpercentage 
+FROM pg_class 
+	LEFT JOIN pg_stat_user_tables 
+		ON pg_stat_user_tables.relid = pg_class.oid 
+	LEFT JOIN gp_toolkit.gp_size_of_table_disk sotd 
+		ON sotd.sotdoid = pg_class.oid 
+	LEFT JOIN gp_toolkit.gp_size_of_table_and_indexes_disk sotaid 
+		ON sotaid.sotaidoid = pg_class.oid 
+	LEFT JOIN gp_toolkit.gp_size_of_table_uncompressed sotu 
+		ON sotu.sotuoid = pg_class.oid 
+	LEFT JOIN pg_namespace 
+		ON pg_namespace.oid = pg_class.relnamespace 
+WHERE 
+	pg_class.relkind = 'r'
+	AND relstorage != 'x' 						
+	AND pg_namespace.nspname NOT IN ('information_schema', 'madlib', 'pg_catalog', 'gptext') 
+	AND pg_class.relname NOT IN ('spatial_ref_sys') 
+ORDER BY 1, 2
