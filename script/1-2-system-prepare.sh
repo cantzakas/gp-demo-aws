7270a4025ab645bccc47365a10cdca3bc07e3e0b
cantzakas <cantzakas@pivotal.io>
2019-09-02 10:26:46 +0100

    AWS VM prep

---
+++ script/1-2-system-prepare.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd $HOME
+
+curl -O https://bootstrap.pypa.io/get-pip.py
+
+python $HOME/get-pip.py
+
+mv $HOME/.bash_profile $HOME/.bash_profile.old
+
+cat >> $HOME/.bash_profile << 'EOF'
+# .bash_profile
+
+# Get the aliases and functions
+if [ -f ~/.bashrc ]; then
+	. ~/.bashrc
+fi
+
+# User specific environment and startup programs
+
+export PATH=$HOME/.local/bin:$HOME/bin:$PATH
+
+EOF
+
+chmod 644 $HOME/.bash_profile
+
+source $HOME/.bash_profile
+
+pip install --upgrade --force-reinstall pip
+
+pip install --upgrade aws-cli
+
+aws --version
