98b113ac6eb6ff119a9521b870a1d2c5b687f5a7
cantzakas <cantzakas@pivotal.io>
2019-09-02 12:52:27 +0100

    remove get-pip.py from directory when finished

--- script/1-2-system-prepare.sh
+++ script/1-2-system-prepare.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 cd $HOME
+
 curl -O https://bootstrap.pypa.io/get-pip.py
 python $HOME/get-pip.py --user
+rm $HOME/get-pip.py
+
 mv $HOME/.bash_profile $HOME/.bash_profile.old
 
 cat >> $HOME/.bash_profile << 'EOF'
@@ -25,5 +28,9 @@ source $HOME/.bash_profile
 pip install --upgrade --force-reinstall --user pip
 pip install --upgrade --force-reinstall --user boto3
 pip install --upgrade --force-reinstall awscli
+
+echo "*********************************************************************************"
+echo " Get AWS CLI version"
+echo "*********************************************************************************"
 aws --version
 
