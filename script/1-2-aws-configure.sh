8b346670ff6b704adb75b1331e4d1220e45f68ba
cantzakas <cantzakas@pivotal.io>
2019-09-02 15:28:57 +0100

    added AWS configuration test

--- script/1-2-aws-configure.sh
+++ script/1-2-aws-configure.sh
@@ -1,3 +1,5 @@
+if [$HOME/.aws]; then rm -Rf $HOME/.aws; fi
+
 mkdir $HOME/.aws
 
 echo "[default]
@@ -9,4 +11,7 @@ region=eu-west-2
 output=json" > $HOME/.aws/config
 
 #test aws config
-aws s3 ls s3://amazon-reviews-pds/tsv/
+if [ "$(aws s3 ls s3://amazon-reviews-pds/tsv/ | awk '{print $4}' | awk 'END {print NR}')" -eq "0" ]; 
+  then echo "Error on AWS S3 Configuration setup"; 
+  else echo "AWS S3 Configuration setup correctly"; 
+fi
