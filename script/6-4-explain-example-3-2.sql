d116ce58d9d4894d3f7ecaf7c26d4f6ca03fd515
cantzakas <cantzakas@pivotal.io>
2019-09-03 16:49:24 +0100

    updates on mpp partitioning, demo 2

--- script/6-4-explain-example-3-2.sql
+++ script/6-4-explain-example-3-2.sql
@@ -4,7 +4,7 @@ cdate_mtd
 FROM demo.amzn_reviews_partitioned, 
 	demo.calendar_mtd 
 WHERE 
-	amzn_reviews.review_date = calendar_mtd.cdate_mtd 
+	amzn_reviews_partitioned.review_date = calendar_mtd.cdate_mtd 
 	AND cdate = '2015-03-25'::DATE 
 GROUP BY cdate_mtd 
 ORDER BY cdate_mtd;
