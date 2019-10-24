02b172f6baa088d3af0ebdf62d3474d7b3ddd1b3
Costas Antzakas <cantzakas@pivotal.io>
2019-10-24 12:15:41 +0000

    updates on create table and gpload scripts

--- script/8-3-copy-data-from-s3.sh
+++ script/8-3-copy-data-from-s3.sh
@@ -11,9 +11,9 @@ cd $S3_DATA_DIR
 ls -laF $S3_DATA_DIR
 
 /usr/local/bin/aws s3 cp s3://uber-movement-ldn/PCD_OA_LSOA_MSOA_LAD_AUG19_UK_LU.csv PCD_OA_LSOA_MSOA_LAD_AUG19_UK_LU.csv
-#/usr/local/bin/aws s3 cp s3://uber-movement-ldn/london-lsoa.json london-lsoa.json
 /usr/local/bin/aws s3 cp s3://uber-movement-ldn/uber-movement-ldn.tar.gz uber-movement-ldn.tar.gz
 /usr/local/bin/aws s3 cp s3://uber-movement-ldn/ukpostcodes.csv ukpostcodes.csv
 /usr/local/bin/aws s3 cp s3://uber-movement-ldn/2018-MRDB-minimal.shp 2018-MRDB-minimal.shp
 /usr/local/bin/aws s3 cp s3://uber-movement-ldn/2018-MRDB-minimal.shx 2018-MRDB-minimal.shx
 /usr/local/bin/aws s3 cp s3://uber-movement-ldn/2018-MRDB-minimal.dbf 2018-MRDB-minimal.dbf
+/usr/local/bin/aws s3 cp s3://uber-movement-ldn/dft_traffic_counts_raw_counts.tar.gz dft_traffic_counts_raw_counts.tar.gz
--- script/8-4-load-postcodes-alter.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE demo.postcode_lookup 
-ALTER COLUMN dointr TYPE INTEGER USING COALESCE(NULLIF(dointr, ''), '0')::INT; 
- 
-ALTER TABLE demo.postcode_lookup 
-ALTER COLUMN doterm TYPE INTEGER USING COALESCE(NULLIF(doterm, ''), '0')::INT; 
- 
-ALTER TABLE demo.postcode_lookup 
-ALTER COLUMN usertype TYPE SMALLINT USING COALESCE(NULLIF(usertype, ''), '0')::SMALLINT;
